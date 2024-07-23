6edc2aabd8399c1f1498eef387cb0ecc3b63fb3d
unknown <brijeshmaurya3112@gmail.com>
2024-07-23 08:20:46 +0530

    email

--- src/main/java/com/home/Service/EmailServiceimpl.java
+++ src/main/java/com/home/Service/EmailServiceimpl.java
@@ -51,4 +51,8 @@ public class EmailServiceimpl implements EmailService {
             throw new RuntimeException(e);
         }
     }
+
+    public static void main(String[] args) {
+        System.out.println("welcome");
+    }
 }
